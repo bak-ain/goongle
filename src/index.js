113b3aeebee3f960a84583869b23275f6d7eef1e
bak-ain <cherryain8452@gmail.com>
2025-05-22 15:51:17 +0900

    index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import './font.css';
-import './common.css'; 
+// import './common.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
